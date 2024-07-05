16a938df23f6b8f5e891aca86610c132893fc889
Alex Nor <advance114@gmail.com>
2024-07-05 21:06:02 +0300

    Assignment for the lesson "Classes"

---
+++ Classes/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Classes
+{
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Подготовка к бою: ");
+            Console.Write("Введите имя бойца: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите начальное здоровье бойца (10-100): ");
+            float health = float.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение брони шлема от 0 до 1: ");
+            float helmArmor = float.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение брони кирасы от 0 до 1: ");
+            float shellArmor = float.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение брони сапог от 0 до 1: ");
+            float bootsArmor = float.Parse(Console.ReadLine());
+
+            Console.Write("Укажите минимальный урон оружия (0-20): ");
+            float minWeaponDamage = float.Parse(Console.ReadLine());
+
+            Console.Write("Укажите максимальный урон оружия (20-40): ");
+            float maxWeaponDamage = float.Parse(Console.ReadLine());
+
+            Unit player = new Unit(name);
+            player.SetHealth(health);
+
+            Helm playerHelm = new Helm("Player's Helm") { ArmorValue = helmArmor };
+            Shell playerShell = new Shell("Player's Shell") { ArmorValue = shellArmor };
+            Boots playerBoots = new Boots("Player's Boots") { ArmorValue = bootsArmor };
+
+            Weapon playerWeapon = new Weapon("Player's Weapon", minWeaponDamage, maxWeaponDamage);
+
+            player.EquipHelm(playerHelm);
+            player.EquipShell(playerShell);
+            player.EquipBoots(playerBoots);
+            player.EquipWeapon(playerWeapon);
+
+
+            Console.WriteLine($"Общий показатель брони равен: {player.Armor}");
+            Console.WriteLine($"Фактическое значение здоровья равно: {player._Health}");
+        }
+
+    }
+}
---
+++ Classes/Unit.cs
@@ -0,0 +1,80 @@
+﻿namespace Classes
+{
+    class Unit(string name)
+    {
+        public string Name { get; } = name;
+        private float health; 
+        public float Health => health;
+        private const float _Damage = 5f;        
+
+        // Конструкторы
+        public Unit() : this("Unknown Unit") { }
+
+        public float _Health => Health * (1f + Armor);
+
+      
+        public bool SetDamage(float value)
+        {          
+            health -= value * (1f - Armor);           
+            return Health <= 0f;
+        }
+
+            public void EquipWeapon(Weapon weapon)
+        {
+            EquippedWeapon = weapon;
+        }
+
+       
+        public void EquipHelm(Helm helm)
+        {
+            EquippedHelm = helm;
+        }
+      
+        public void EquipShell(Shell shell)
+        {
+            EquippedShell = shell;
+        }
+       
+        public void EquipBoots(Boots boots)
+        {
+            EquippedBoots = boots;
+        }
+       
+        public float Damage
+        {
+            get
+            {               
+                if (EquippedWeapon != null)
+                    return EquippedWeapon.GetDamage() + _Damage; 
+                else
+                    return _Damage; 
+            }
+        }
+       
+        public float Armor
+        {
+            get
+            {
+                float totalArmor = 0f;               
+                if (EquippedHelm != null)
+                    totalArmor += EquippedHelm.ArmorValue;
+                if (EquippedShell != null)
+                    totalArmor *= EquippedShell.ArmorValue;
+                if (EquippedBoots != null)
+                    totalArmor += EquippedBoots.ArmorValue;
+                return Math.Min(totalArmor, 1f);
+            }
+        }
+       
+        public void SetHealth(float initialHealth)
+        {
+            health = initialHealth;
+        }
+
+        // Предметы персонажа
+        private Weapon EquippedWeapon { get; set; }
+        private Helm EquippedHelm { get; set; }
+        private Shell EquippedShell { get; set; }
+        private Boots EquippedBoots { get; set; }
+    }
+}
